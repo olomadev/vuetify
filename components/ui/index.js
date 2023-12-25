31331ceb779fda2f3301a840e01a00eefbe6f291
Ersin Güvenç <eguvenc@gmail.com>
2023-12-25 12:30:23 +0300

    added missed ui/index.js, fixed bug

---
+++ components/ui/index.js
@@ -0,0 +1,24 @@
+/*!
+ * Oloma Dev.
+ * [oloma-va] <https://github.com/olomadev/oloma-vuetify>
+ *
+ * Copyright (c) 2022-2024, Oloma Software.
+ */
+
+// Advanced components
+// 
+import ArrayTableInput from "./inputs/ArrayTableInput";
+import CheckListInput from "./inputs/CheckListInput";
+import CurrencyInput from "./inputs/CurrencyInput";
+import AvatarInput from "./inputs/AvatarInput";
+import SheetInput from "./inputs/SheetInput";
+import VueCurrencyComponent from "./inputs/VueCurrencyComponent";
+
+export {
+  ArrayTableInput,
+  AvatarInput,
+  CheckListInput,
+  CurrencyInput,
+  SheetInput
+  VueCurrencyComponent,
+};
